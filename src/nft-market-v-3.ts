67ed35d7f4ca63922df9310089899874f9f2690c
xszxc <2112067692@qq.com>
2025-03-05 00:48:15 +0800

    自定义要采集的数据

--- src/nft-market-v-3.ts
+++ src/nft-market-v-3.ts
@@ -14,10 +14,13 @@ import {
   OwnershipTransferred,
   SetFeeTo,
   SetWhiteListSigner,
-  Sold
+  Sold,
+  OrderBook,
+  FilledOrder
 } from "../generated/schema"
 
 export function handleCancel(event: CancelEvent): void {
+  // 数据ID： 交易哈希 + 事件日志索引
   let entity = new Cancel(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
@@ -26,6 +29,12 @@ export function handleCancel(event: CancelEvent): void {
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
+
+  let orderBook = OrderBook.load(entity.orderId)
+  if (orderBook != null) {
+    orderBook.cancelTxHash = entity.id
+    orderBook.save()
+  }
 
   entity.save()
 }
@@ -59,6 +68,18 @@ export function handleList(event: ListEvent): void {
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
+
+  let orderBook = new OrderBook(event.params.orderId)
+  orderBook.nft = entity.nft
+  orderBook.tokenId = entity.tokenId
+  orderBook.seller = entity.seller
+  orderBook.payToken = entity.payToken
+  orderBook.price = entity.price
+  orderBook.deadline = entity.deadline
+  orderBook.blockNumber = entity.blockNumber
+  orderBook.blockTimestamp = entity.blockTimestamp
+  orderBook.transactionHash = entity.transactionHash
+  orderBook.save()
 
   entity.save()
 }
@@ -116,6 +137,22 @@ export function handleSold(event: SoldEvent): void {
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
+
+  let orderBook = OrderBook.load(entity.orderId)
+  if (orderBook != null) {
+    orderBook.filledTxHash = entity.id
+    orderBook.save()
+  }
+
+  let filledOrder = new FilledOrder(entity.orderId)
+  filledOrder.buyer = entity.buyer
+  filledOrder.fee = entity.fee
+  filledOrder.blockNumber = entity.blockNumber
+  filledOrder.blockTimestamp = entity.blockTimestamp
+  filledOrder.transactionHash = entity.transactionHash
+  filledOrder.order = orderBook ? orderBook.id : null
+  filledOrder.save()
+
 
   entity.save()
 }
